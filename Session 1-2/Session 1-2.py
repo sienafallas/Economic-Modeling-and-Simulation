35f20f5ded525772c847551ae3a857f79f08b794
sienafallas <sienaquelhas@gmail.com>
2024-03-15 11:13:58 +0100

    g

--- Session 1-2/Session 1-2.py
+++ Session 1-2/Session 1-2.py
@@ -1,8 +1,9 @@
 class Point:
-    def __init__(self):
-    x = 2
-    y = 3
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-a = Point()
-
+a = Point(2, 3)
+b = Point (7, 9)
 print(f"a=({a.x}, {a.y})")
+print(f"b=({b.x}, {b.y})")
